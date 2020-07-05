2fd6c4b85ba035dcf6d59d0b00da0bd37f140e07
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-07-05 09:45:30 +0530

    Modified to check prefix

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -6,12 +6,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
-		// fmt.Println("response: ", resp)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
